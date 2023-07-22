bf52a4772b2c253155365aabdfb04b327fda924c
WebSpruce <kacperswi12363@gmail.com>
2023-07-22 12:29:46 +0200

    Add project files.

---
+++ mauiOperationsOnObjects/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace mauiOperationsOnObjects;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ mauiOperationsOnObjects/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace mauiOperationsOnObjects;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
